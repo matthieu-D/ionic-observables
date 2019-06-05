2cba06fe99e3044d74006e6bb19d0ec264b4e5c5
e <e>
2019-06-05 19:30:16 +0200

    all working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Observable, Observer, Subscription, fromEvent } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+import { Http } from '@angular/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,63 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  observable: Observable<string>;
+  observer: Observer<string>;
+  subscription: Subscription;
 
+  constructor(public http: Http) { }
+
+  ngOnInit() {
+    this.createOwnFlow();
+    this.connectToApi();
+    this.subscribeToMouse();
+  }
+
+  createOwnFlow() {
+    this.observable = new Observable((observer: Observer<string>) => {
+      this.observer = observer;
+    });
+
+    this.observable
+      .subscribe(this.handleData, this.handleError , this.handleComplete);
+
+    this.observer.next('12');
+    this.observer.next('15');
+    this.observer.complete();
+    this.observer.next('16');
+  }
+
+  connectToApi() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .pipe(map(this.extractData))
+      .subscribe(this.handleData, this.handleError, this.handleComplete);
+  }
+
+  subscribeToMouse() {
+    this.subscription = fromEvent(document, 'mousemove')
+      .subscribe(this.handleData, this.handleError, this.handleComplete);
+  }
+
+  extractData(response) {
+    const body = response.json();
+    return body || { };
+  }
+
+  handleData(data) {
+    console.log('Here are the usable data', data);
+    // Insert Business logic here
+  }
+
+  handleComplete() {
+    console.log('Complete');
+  }
+
+  handleError(error) {
+    console.log('error:', error)
+    return Observable.throw(error);
+  }
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
